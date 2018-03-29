c41571f9259d70b671d76a5bbc80468fdc56cedd
Alexander <ego.cogitare@gmail.com>
2018-03-29 22:15:24 +0300

    ...

--- app/Http/Controllers/Api/EventsController.php
+++ app/Http/Controllers/Api/EventsController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class EventsController extends Controller
 {
+    const RIDE_CATEGORY_IDS = [4];
+    
     /**
      * Display a listing of the resource.
      *
@@ -16,6 +20,11 @@ class EventsController extends Controller
      */
     public function index(Request $request)
     {
+        $date        = $request->input('date');
+        $category_id = $request->input('category');
+        $location    = $request->input('location');
+        $destination = $request->input('destination');
+        
         $result = [];
         
         for ($i = 1; $i >= 0; $i--) 
@@ -25,9 +34,39 @@ class EventsController extends Controller
                 }])
                 ->where('is_top', $i)
                 ->where('is_active', 1)
-                ->where('date', '>', date('Y-m-d H:i:s'))
-                ->orderBy('date', 'ASC')
-                ->get();
+                ->where('date', '>', date('Y-m-d H:i:s'));
+                
+            // Filter parameters (apply only for none top events)
+            if ($i === 0) 
+            {
+                $categories = [];
+                
+                // Get all children categories for the category
+                if (!empty($category_id)) {
+                    $categories = Category::where([
+                        'parent_id' => $category_id,
+                        'is_active' => 1
+                    ])
+                    ->pluck('id')
+                    ->toArray();
+                    
+                    $categories[] = (int)$category_id;
+                }
+                
+                // Filtering by date
+                !empty($date) && $events->where('date', 'LIKE', Carbon::createFromFormat('d.m.Y', $date)->format('Y-m-d') . '%');
+                
+                // Filter by category and all child categories
+                !empty($category_id) && $events->whereIn('category_id', $categories);
+                
+                // Filter by event location
+                !empty($location) && $events->where('event_location_human', 'LIKE', $location);
+                
+                // Filter by destination location only for "Reise" category
+                !empty($destination) && in_array($category_id, self::RIDE_CATEGORY_IDS) && $events->where('event_destination_human', $destination);
+            }
+            
+            $events = $events->orderBy('date', 'ASC')->get();
 
             foreach ($events as $event) {
                 $parent_category = $event->category->parent 
